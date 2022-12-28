a939b4f8ce649c2c904260883d1acc81fe78fa2c
Ford <wbc123454321@gmail.com>
2022-12-28 20:43:15 +0600

    Initial1

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/firstGit/FirstGit.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/firstGit/FirstGit.java
@@ -3,5 +3,6 @@ package com.javarush.task.task37.task3707.firstGit;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println();
     }
 }
